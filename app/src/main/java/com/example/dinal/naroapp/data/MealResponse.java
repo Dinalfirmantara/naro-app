640137587ede2c0ac683b7d6bbd9167ed433593f
Dinal Firmantara <dinalf15@gmail.com>
2018-11-22 10:31:14 +0700

    add meals response model

---
+++ app/src/main/java/com/example/dinal/naroapp/data/MealResponse.java
@@ -0,0 +1,27 @@
+package com.example.dinal.naroapp.data;
+
+import java.util.List;
+import com.google.gson.annotations.SerializedName;
+
+
+public class MealResponse{
+
+	@SerializedName("meals")
+	private List<MealsItem> meals;
+
+	public void setMeals(List<MealsItem> meals){
+		this.meals = meals;
+	}
+
+	public List<MealsItem> getMeals(){
+		return meals;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"MealResponse{" + 
+			"meals = '" + meals + '\'' + 
+			"}";
+		}
+}
